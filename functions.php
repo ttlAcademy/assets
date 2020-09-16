4ce34e9a50ac44175546daa1b0a2a07f52c28ce2
ikoral <ilhankoral@outlook.com>
2020-09-17 00:28:14 +0200

    woocommerce before_calculate_totals added

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
+
 <?php
+
+// 
 $theme_info = wp_get_theme();
-define('STM_THEME_VERSION', ( WP_DEBUG ) ? time() : $theme_info->get( 'Version' ) );
-define('STM_MS_SHORTCODES', '1' );
+define('STM_THEME_VERSION', (WP_DEBUG) ? time() : $theme_info->get('Version'));
+define('STM_MS_SHORTCODES', '1');
 
 $inc_path = get_template_directory() . '/inc';
 
@@ -19,7 +22,7 @@ require_once($inc_path . '/theme-config.php');
 
 // Visual composer custom modules
 if (defined('WPB_VC_VERSION')) {
-        require_once($inc_path . '/visual_composer.php');
+    require_once($inc_path . '/visual_composer.php');
 }
 
 // Custom code for any outputs modifying
@@ -28,64 +31,69 @@ require_once($inc_path . '/custom.php');
 
 // Custom code for woocommerce modifying
 if (class_exists('WooCommerce')) {
-        require_once($inc_path . '/woocommerce_setups.php');
+    require_once($inc_path . '/woocommerce_setups.php');
 }
 
-if(defined('STM_LMS_URL')) {
-        require_once($inc_path . '/lms/main.php');
+if (defined('STM_LMS_URL')) {
+    require_once($inc_path . '/lms/main.php');
 }
-function stm_glob_pagenow(){
+function stm_glob_pagenow()
+{
     global $pagenow;
     return $pagenow;
 }
-function stm_glob_wpdb(){
+function stm_glob_wpdb()
+{
     global $wpdb;
     return $wpdb;
 }
 
-if(class_exists('BuddyPress')) {
+if (class_exists('BuddyPress')) {
     require_once($inc_path . '/buddypress.php');
 }
 
 //Announcement banner
 if (is_admin()) {
-        require_once($inc_path . '/admin/generate_styles.php');
-        require_once($inc_path . '/admin/admin_helpers.php');
-        require_once($inc_path . '/admin/review/review-notice.php');
-        require_once($inc_path . '/admin/product_registration/admin.php');
-        require_once($inc_path . '/tgm/tgm-plugin-registration.php');
+    require_once($inc_path . '/admin/generate_styles.php');
+    require_once($inc_path . '/admin/admin_helpers.php');
+    require_once($inc_path . '/admin/review/review-notice.php');
+    require_once($inc_path . '/admin/product_registration/admin.php');
+    require_once($inc_path . '/tgm/tgm-plugin-registration.php');
 }
 
 // Change return to shop text
-add_filter( 'gettext', 'change_woocommerce_return_to_shop_text', 20, 3 );
+add_filter('gettext', 'change_woocommerce_return_to_shop_text', 20, 3);
 
-function change_woocommerce_return_to_shop_text( $translated_text, $text, $domain ) {
-        switch ( $translated_text ) {
-        case 'Return to shop' :
-                $translated_text = __( 'Return to Courses', 'woocommerce' );
-                break;
-        }
-        return $translated_text;
+function change_woocommerce_return_to_shop_text($translated_text, $text, $domain)
+{
+    switch ($translated_text) {
+        case 'Return to shop':
+            $translated_text = __('Return to Courses', 'woocommerce');
+            break;
+    }
+    return $translated_text;
 }
 
 // Change return to shop URL
 
-function wc_empty_cart_redirect_url() {
+function wc_empty_cart_redirect_url()
+{
     return 'https://www.ttl.academy/courses/';
 }
-add_filter( 'woocommerce_return_to_shop_redirect', 'wc_empty_cart_redirect_url' );
+add_filter('woocommerce_return_to_shop_redirect', 'wc_empty_cart_redirect_url');
 
 
 //Limit Cart number to 1
 
-add_filter( 'woocommerce_add_to_cart_validation', 'only_one_items_allowed_add_to_cart', 10, 3 );
+add_filter('woocommerce_add_to_cart_validation', 'only_one_items_allowed_add_to_cart', 10, 3);
 
-function only_one_items_allowed_add_to_cart( $passed, $product_id, $quantity ) {
+function only_one_items_allowed_add_to_cart($passed, $product_id, $quantity)
+{
     $cart_items_count = WC()->cart->get_cart_contents_count();
-    
-    if( $cart_items_count >= 1){
+
+    if ($cart_items_count >= 1) {
         // Set to false
-        $cart_items_count =1;
+        $cart_items_count = 1;
         $passed = false;
         // Display a message
         //wc_add_notice( __( "You can’t have more than 1 same course in cart", "woocommerce" ), "error" );
@@ -93,21 +101,51 @@ function only_one_items_allowed_add_to_cart( $passed, $product_id, $quantity ) {
     return $passed;
 }
 
-add_filter( 'woocommerce_update_cart_validation', 'only_one_items_allowed_cart_update', 10, 4 );
-function only_one_items_allowed_cart_update( $passed, $cart_item_key, $values, $updated_quantity ) {
+add_filter('woocommerce_update_cart_validation', 'only_one_items_allowed_cart_update', 10, 4);
+function only_one_items_allowed_cart_update($passed, $cart_item_key, $values, $updated_quantity)
+{
     $cart_items_count = WC()->cart->get_cart_contents_count();
     $original_quantity = $values['quantity'];
     $total_count = $cart_items_count - $original_quantity + $updated_quantity;
 
-    if( $cart_items_count > 1 || $total_count > 1 ){
-            // Set to false
-            if( $updated_quantity > 1 ){
-                $passed = false;
+    if ($cart_items_count > 1 || $total_count > 1) {
+        // Set to false
+        if ($updated_quantity > 1) {
+            $passed = false;
 
             // Display a message
-                wc_add_notice( __( "You can’t add more than one same course in cart", "woocommerce" ), "error" );
-            }
+            wc_add_notice(__("You can’t add more than one same course in cart", "woocommerce"), "error");
+        }
     }
     return $passed;
 }
 
+add_action('woocommerce_before_calculate_totals', 'change_cart_item_quantities', 20, 1);
+function change_cart_item_quantities($cart)
+{
+
+    if (did_action('woocommerce_before_calculate_totals') >= 2)
+        return;
+
+    // HERE below define your specific products IDs
+
+    $new_qty = 1;
+    // Checking cart items
+    foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+        //$product_id = $cart_item['data']->get_id();
+        // Check for specific product IDs and change quantity
+        // if( in_array( $product_id, $specific_ids ) && $cart_item['quantity'] != $new_qty ){
+        $cart->set_quantity($cart_item_key, $new_qty); // Change quantity
+    }
+    //}
+}
+
+
+function defer_parsing_of_js($url)
+{
+    if (is_user_logged_in()) return $url; //don't break WP Admin
+    if (FALSE === strpos($url, '.js')) return $url;
+    if (strpos($url, 'jquery.js')) return $url;
+    return str_replace(' src', ' defer src', $url);
+}
+add_filter('script_loader_tag', 'defer_parsing_of_js', 10);
